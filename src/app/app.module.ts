090a0e34f00e6903850e2a3a584ebae7495d46da
Sh0o7eR <manuel.midiri@hotmail.it>
2021-05-05 10:05:48 +0200

    Structural project + setting interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './shared/home-page/home-page.component';
+import { DetailsComponent } from './shared/details/details.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
