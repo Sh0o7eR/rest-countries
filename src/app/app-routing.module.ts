af1fac6ae9dc918d8d6fa7d07389bd9ef6b72d85
Sh0o7eR <manuel.midiri@hotmail.it>
2021-05-05 15:37:35 +0200

    styling details countries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from './shared/home-page/home-page.component';
+import {DetailsComponent} from './shared/details/details.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'details', component: DetailsComponent}
 ];
 
 @NgModule({
